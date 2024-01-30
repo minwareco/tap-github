8e3d080cb3a403c978ecf3f52bd31403a8a745e5
Alex Boyd <859286+aaboyd@users.noreply.github.com>
2024-01-30 15:00:15 -0500

    Update PyJWT and cryptography for security vulnerability fixes (#49)

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ setup(name='tap-github',
           'requests==2.20.0',
           'psutil==5.8.0',
           'debugpy==1.5.1',
-          'PyJWT==2.3.0',
-          'cryptography==36.0.2',
+          'PyJWT==2.8.0',
+          'cryptography==42.0.1',
           'gitlocal@git+https://{}@github.com/minwareco/gitlocal.git'.format(os.environ.get("GITHUB_TOKEN", ""))
       ],
       extras_require={
