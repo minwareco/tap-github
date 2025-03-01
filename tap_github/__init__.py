15ce565cf43f00ffcaeb77d8bba3823b851a432f
Myles Henderson <myles@minware.com>
2025-03-01 07:57:52 -0500

    Myles/mw 5522 update using pat logic (#79)
    * update using pat logic
    
    * updates

--- tap_github/__init__.py
+++ tap_github/__init__.py
@@ -39,6 +39,8 @@ graphql_url = ''
 session = requests.Session()
 logger = SecureLogger(singer.get_logger())
 
+using_pat = False
+
 REQUIRED_CONFIG_KEYS = ['start_date', 'access_token', 'repository']
 
 KEY_PROPERTIES = {
@@ -306,12 +308,6 @@ def authed_get(source, url, headers={}, overrideMethod='get', data=None):
         just_refreshed_token = False
         network_retry_count = 0
         network_max_retries = 5
-        
-        # Check if we're using a PAT or GitHub App
-        using_pat = False
-        auth_header = session.headers.get('authorization', '')
-        if auth_header.startswith('token '):
-            using_pat = True
         
         while True:
             try:
@@ -630,11 +626,13 @@ def getOrgs():
     return orgs
 
 def set_auth_headers(config, org = None):
+    global using_pat
     access_token = config['access_token']
 
     # If we don't have a personal access token, use the github app to get an installation access
     # token
     if not access_token or len(access_token) == 0:
+        using_pat = False
         if not org:
             raise Exception('Org value must be provided when authorizing with an app installation key')
         elif org in cached_app_tokens:
@@ -643,6 +641,7 @@ def set_auth_headers(config, org = None):
         appid = config['app_id']
         access_token = refresh_app_token(pem, appid, org)
     else:
+        using_pat = True
         session.headers.update({'authorization': 'token ' + access_token})
 
     logger.addToken(access_token)
@@ -2844,6 +2843,8 @@ def do_sync(config, state, catalog):
         # right now and running out of memory as a result.
         singer.write_state(state)
 
+
+
 def main():
     global latest_response
     global latest_request
@@ -2851,7 +2852,7 @@ def main():
     args = singer.utils.parse_args(REQUIRED_CONFIG_KEYS)
     if args.config and 'access_token' in args.config:
         logger.addToken(args.config['access_token'])
-
+        
     try:
         if args.discover:
             do_discover(args.config)
